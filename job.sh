70fd7b94fdefb89e3b7cd492b340733cdd966d1e
tony <tony92151@gmail.com>
2019-11-29 06:37:37 +0000

    job

---
+++ job.sh
@@ -0,0 +1,25 @@
+cd /root/notebooks/imet/iMet-Collection-2019-FGVC6
+
+bash /root/notebooks/install_tf2.sh
+
+pip install runipy
+
+pip install torchsummary
+
+pip install pretrainedmodels
+
+pip install numpy==1.17.3
+
+pip install albumentations
+
+pip install prefetch_generator
+
+#python imet_top_solution.py
+
+#runipy runipy.ipynb OutputNotebook.ipynb
+
+#runipy runipy.ipynb
+
+#cp ~/se_resnext101_32x4d-3b2fe3d8.pth /home/aiforge/.cache/torch/checkpoints/se_resnext101_32x4d-3b2fe3d8.pth
+
+runipy  -o imet_top_solution3.ipynb
